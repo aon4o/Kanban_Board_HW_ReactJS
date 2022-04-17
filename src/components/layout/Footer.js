9437f13232076577ae9461227377b6aa18e2a7ca
Kristiyan Petrov <petrov.kris03@gmail.com>
2022-04-17 18:01:15 +0300

    Frontend reworked

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,43 +6,43 @@ const Footer = () => {
         <footer>
             <Container className="d-flex flex-wrap justify-content-between align-items-center py-3">
                 <div className="col-md-4 d-flex align-items-center">
-                    <a className="mb-3 me-3 mb-md-0 lh-1" href={"https://github.com/aon2003"}>
+                    <a className="mb-3 me-3 mb-md-0 lh-1 text-primary" href={"https://github.com/aon2003/Kanban_Board_HW_ReactJS"}>
                         <FaGithub/>
                     </a>
-                    <a className="mb-3 me-3 mb-md-0 lh-1" href="/#">
+                    <a className="mb-3 me-3 mb-md-0 lh-1 text-primary" href="/#">
                         <FaCode/>
                     </a>
-                    <span>
-                        Kanban Board HW
+                    <span className={'text-light'}>
+                        Umrello
                     </span>
                 </div>
 
                 <div className="justify-content-center">
-                    <span>
-                        {/*Made with <FaHeart/> and <FaCoffee/> by <a className="text-muted" href="https://www.linkedin.com/in/alex-naida/">Alex Naida</a>*/}
+                    <span className={'text-light'}>
+                        Made by <a className="text-primary" href="https://www.linkedin.com/in/alex-naida/">Alex Naida</a> && <a className='text-primary' href='https://www.linkedin.com/in/kris-petrov/'>Yan Petrov</a>
                     </span>
                 </div>
 
                 <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
                     <li>
-                        <span>ELSYS / TUES</span>
+                        <span className={'text-light'}>ELSYS / TUES</span>
                     </li>
                     <li className="ms-3">
-                        <a href={"https://www.tues.bg/"} rel="noopener noreferrer" target="_blank">
+                        <a href={"https://www.tues.bg/"} rel="noopener noreferrer" target="_blank" className={'text-primary'}>
                             <FaGlobe/>
                         </a>
                     </li>
                     <li className="ms-3">
-                        <a href={"https://www.instagram.com/tues.bg/"} rel="noopener noreferrer" target="_blank">
+                        <a href={"https://www.instagram.com/tues.bg/"} rel="noopener noreferrer" target="_blank" className={'text-primary'}>
                             <FaInstagram/>
                         </a>
                     </li>
                     <li className="ms-3">
-                        <a href={"https://www.linkedin.com/school/tues/"} rel="noopener noreferrer" target="_blank">
+                        <a href={"https://www.linkedin.com/school/tues/"} rel="noopener noreferrer" target="_blank" className={'text-primary'}>
                             <FaLinkedin/>
                         </a>
                     </li>
-                </ul>
+                </ul>кара
             </Container>
         </footer>
     )
